5fec89401a10f196780e8c737498bf85201d8ff1
Darren <darren2097@gmail.com>
2022-08-22 16:58:13 +0200

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
- * list: singly linked list
+ * @list: singly linked list
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
