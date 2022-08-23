46e9745a0ab118dc2a8a6a9f4ca04c1c140a212d
Darren <darren2097@gmail.com>
2022-08-23 21:46:31 +0200

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -7,4 +7,4 @@ for i in range(122, 110, -1):
     print("{}".format(chr(letter)), end='')
     letter = letter + 31
 print("{}".format(chr(98)), end='')
-print("{}".format(chr(65)))
+print("{}".format(chr(65)), end='')
