d3fd6921d6a0c92c9f9a98519429f6f30f558846
Darren <darren2097@gmail.com>
2022-08-26 15:58:18 +0200

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    for i in range(rows):
+        for j in range(columns):
+            print("{:d}".format(matrix[i][j]), end=" ")
+
+    print()
