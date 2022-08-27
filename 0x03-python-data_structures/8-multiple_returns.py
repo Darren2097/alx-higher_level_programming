e288c1abde956b6dd504130c8cee1ab82142bfdb
Darren <darren2097@gmail.com>
2022-08-27 12:00:31 +0200

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    sent_len = len(sentence)
 
     if len(sentence) == 0:
-        return None
+        return (sent_len, None)
     else:
-        sent_len = len(sentence)
         first_str = sentence[0]
         return (sent_len, first_str)
