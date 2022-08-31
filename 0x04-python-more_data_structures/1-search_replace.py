a1cf77384e9047401bd5b891fc8832f4a5b3e627
Darren <darren2097@gmail.com>
2022-08-31 19:55:12 +0200

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for i in len(new_list):
+        if new_list[i] == search:
+            new_list[len(i):] = replace
+        else:
+            new_list[len(i):] = search
+
+    return (new_list)
