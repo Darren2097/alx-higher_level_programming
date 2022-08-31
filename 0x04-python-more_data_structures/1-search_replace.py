92e09ce9a860873e358052f1112c8a1c6c162cbc
Darren <darren2097@gmail.com>
2022-08-31 20:04:03 +0200

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     new_list = []
 
     for i in my_list:
-        if my_list[i] == search:
+        if i == search:
             new_list.append(replace)
         else:
             new_list.append(search)
