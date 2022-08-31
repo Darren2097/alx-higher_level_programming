411defdaf04431c7b2c4a2098124cdc1e007e7fb
Darren <darren2097@gmail.com>
2022-08-31 21:40:28 +0200

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,13 @@
 def roman_to_int(roman_string):
     if roman_string is not None and type(roman_string) == str:
         rom_dict = {
-                "I" = 1,
-                "V" = 5,
-                "X" = 10,
-                "L" = 50,
-                "C" = 100,
-                "D" = 500,
-                "M" = 1000
+                'I' = 1,
+                'V' = 5,
+                'X' = 10,
+                'L' = 50,
+                'C' = 100,
+                'D' = 500,
+                'M' = 1000
                 }
 
         result = 0
