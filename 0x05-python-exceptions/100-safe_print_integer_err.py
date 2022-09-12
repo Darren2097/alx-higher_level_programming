04dcb2ca4a700017d6aefe85ac5eb3b8438ffb0e
Darren <darren2097@gmail.com>
2022-09-12 20:37:32 +0200

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,6 +8,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
-        sys.stderr.write("Exception: \n")
+    except Exception as ex:
+        ex = "Exception " + str(ex) + '\n'
+        sys.stderr.write(ex)
         return False
