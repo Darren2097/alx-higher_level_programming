4ea6fd925effb847371f197dae0a4505eb58a7c6
Darren <darren2097@gmail.com>
2022-09-13 17:19:26 +0200

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,11 +8,6 @@ class Square:
     def __init__(self, size=0):
         """Initializes a __init__ method that sets the size of a square"""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
         self.__size = size
 
     def area(self):
