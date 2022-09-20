06e79b6fc3705b65d1ba3d6e82b0c014a54c2c33
Darren <darren2097@gmail.com>
2022-09-20 09:36:47 +0200

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a __init__ method that sets the size of a rectangle"""
-        
+
         self.width = width
         self.height = height
 
@@ -25,12 +25,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """returns height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """sets the height"""
@@ -46,15 +46,15 @@ class Rectangle:
 
     def perimeter(self):
         """returns perimeter of rectangle"""
-        
+
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return ((2 * self.width) + (2 * self.height))
-    
+
     def __str__(self):
         """prints the rectangle with the character #"""
-        
+
         string = ''
         if self.width == 0 or self.height == 0:
             return string
@@ -67,4 +67,4 @@ class Rectangle:
 
     def __repr__(self):
         """returns a string representation of the rectangle"""
-        return ("Rectangle({:d}, {:d}".format(self.width, self.height))
+        return ("Rectangle({:d}, {:d})".format(self.width, self.height))
