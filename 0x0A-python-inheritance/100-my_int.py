c3bbea016ba392939fccf88e5e0b764465218676
Darren <darren2097@gmail.com>
2022-09-26 21:35:11 +0200

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,10 +5,16 @@
 class MyInt(int):
     """MyInt class that does the opposite"""
 
-    def __equal__(self, op):
-        """gets the opposite of equal(__equal__)"""
-        return super().__nequal__(op)
+    def __eq__(self, op):
+        """gets the opposite of equi"""
+        return super().__ne__(op)
 
-    def __nequal__(self, op):
-        """gets the opposite of non-equal(__nequal__)"""
-        return super().__equal__(op)
+    def __ne__(self, op):
+        """gets the opposite of nonequi"""
+        return super().__eq__(op)
+
+if __name__ == '__main__':
+    my_i = MyInt(3)
+    print(my_i)
+    print(my_i == 3)
+    print(my_i != 3)
