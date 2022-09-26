7854c2352aff6d3aa8065f17645564608889cbc4
Darren <darren2097@gmail.com>
2022-09-26 20:07:20 +0200

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,7 @@ def inherits_from(obj, a_class):
     """inherits_from method"""
 
     if issubclass(type(obj), a_class):
-        return True
+        if type(obj) is not a_class:
+            return True
     else:
         return False
