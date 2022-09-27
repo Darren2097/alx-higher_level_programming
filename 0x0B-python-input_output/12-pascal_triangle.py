5d0ca9cf0d195041dc68a500f9f4da3a089d82e0
Darren <darren2097@gmail.com>
2022-09-27 21:56:41 +0200

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -22,12 +22,3 @@ def pascal_triangle(n):
                     triangle[r][c] = triangle[r - 1][c - 1] + triangle[r-1][c]
 
     return triangle
-
-if __name__ == '__main__':
-    def print_triangle(triangle):
-        """Print the triangle"""
-                         
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(8))
