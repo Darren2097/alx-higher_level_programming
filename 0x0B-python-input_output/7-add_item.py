3c9b8d6cfa48f64436b2548ebcf14edf8ca226fb
Darren <darren2097@gmail.com>
2022-09-27 21:07:11 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ import sys
 import json
 import os
 
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -14,9 +15,9 @@ if not os.path.isfile(filename):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write("[]")
 
-if len(argv) > 1:
+if len(sys.argv) > 1:
     file_data = load_from_json_file(filename)
-    for i in range(1, len(argv)):
+    for i in range(1, len(sys.argv)):
         file_data.append(sys.argv[i])
 
     save_to_json_file(file_data, filename)
