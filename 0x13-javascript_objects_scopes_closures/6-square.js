4b0977fb1083afa52b26c93ef4eb246d395de62a
Darren <darren2097@gmail.com>
2022-11-29 20:27:30 +0200

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const NewSquare = require('./5-square');
+
+class Square extends NewSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c += 'X';
+    }
+
+    let rows, columns;
+
+    for (rows = 0; rows < this.height; rows++) {
+      let str = '';
+      for (columns = 0; columns < this.width; columns++) {
+        str += c;
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Square;
