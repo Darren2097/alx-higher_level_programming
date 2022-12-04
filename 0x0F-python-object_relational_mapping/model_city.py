67ca14258e84e99d16d45820dfe99e0fcaad9136
Darren <darren2097@gmail.com>
2022-12-04 19:31:21 +0200

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,8 @@ from model_state import State, Base
 
 
 class City(Base):
+    """City class inherited from Base"""
+
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
